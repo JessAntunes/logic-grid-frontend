476c57f69694f1fcf8f6d7f50ac2556b9456a30a
Jessica Antunes <jessantunes@protonmail.com>
2021-06-15 01:40:43 -0400

    change api url

--- javascript/services/API.js
+++ javascript/services/API.js
@@ -1,15 +1,13 @@
 class API {
 
     static addGames() {
-        fetch("http://localhost3000/games")
+        fetch("http://127.0.0.1:3000/games")
         .then(response => response.json())
-        .then(games => {
-            games.forEach( game => {
+        .then(game => {
                 const {id, title, discription, clues, solution, top_header, top_label, side_header, side_label} = game
-                new Game(id, title, discription, clues, solution, top_header, top_label, side_header, side_label)
+                // new Game(id, title, discription, clues, solution, top_header, top_label, side_header, side_label)
             })
-        })
-    }
+        }
+   
 }
 
-    
