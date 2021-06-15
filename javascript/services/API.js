1eb5481319f9aecb52459432e9d99cc7fe8a332e
Jessica Antunes <jessantunes@protonmail.com>
2021-06-15 01:00:17 -0400

    add api class and javascript/services folders

---
+++ javascript/services/API.js
@@ -0,0 +1,12 @@
+class API {
+
+    static addGames() {
+        fetch("http://localhost3000/games")
+        .then(response => response.json())
+        .then(games => {
+            games.forEach( game => {
+                const {id, title, discription, clues, solution, top_header, top_label, side_header, side_label} = game
+                new Game(id, title, discription, clues, solution, top_header, top_label, side_header, side_label)
+            })
+        })
+    }
